0fd06cd4f93e1936f5088cbd5d5ba418cb9257d3
Manny Dheer <mannydheer@gmail.com>
2020-06-01 18:40:04 -0400

    feat: sprites implementation

--- Engine.js
+++ Engine.js
@@ -5,19 +5,6 @@ class Engine {
 
   //Just using a simple boolean is not efficient because it doesn't take into account the different
   //speeds that a computer can run the program.
-  gameLoop(timeStamp) {
-    const canvas = document.getElementById("gameCanvas");
-    const ctx = canvas.getContext("2d");
-    let lastTime = 0;
-    let deltaTime;
-
-    ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
-    deltaTime = timeStamp - lastTime;
-    // this.lastTime = timeStamp;
-    // this.player.draw(this.ctx);
-    // this.player.update(this.deltaTime);
-    window.requestAnimationFrame(this.gameLoop);
-  }
 
   //display background.
 }
--- game.js
+++ game.js
@@ -1,6 +1,29 @@
+// var canvas = document.getElementById("gameCanvas");
+// //rendering context to draw to the element.
+// var ctx = canvas.getContext("2d");
+
+//Just using a simple boolean is not efficient because it doesn't take into account the different
+//speeds that a computer can run the program.
+var canvas = document.getElementById("gameCanvas");
 //rendering context to draw to the element.
-//need to clear canvas as new canvas gets re-rendered.
+var ctx = canvas.getContext("2d");
 let gameEngine = new Engine();
-window.onload = function () {
-  window.requestAnimationFrame(gameEngine.gameLoop);
+//player class.
+let player = gameEngine.player;
+let lastTime;
+//-------------------GAME LOOP-------------------
+const gameLoop = (timeStamp) => {
+  ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
+  //time for
+  let deltaTime = timeStamp - lastTime;
+  lastTime = timeStamp;
+  player.draw(ctx);
+  player.update(deltaTime);
+  window.requestAnimationFrame(gameLoop);
 };
+window.requestAnimationFrame(gameLoop);
+
+// ----------------SPRITES ---------------------
+// let tree = (document.getElementById(
+//   "image"
+// ).style.backgroundPosition = `-256px 0px`);
